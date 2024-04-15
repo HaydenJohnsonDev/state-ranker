90aae73d46ccdd66ce2fb2d8236bbdce044e1e4e
Hayden Johnson <HaydenJohnsonDev>
2024-04-14 19:03:47 -0600

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,304 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Checkbox,
+  Divider,
+  Input,
+} from "@nextui-org/react";
+import { states } from "@/lib/content/states";
+
+interface State {
+  stateName: string;
+  conservative: number;
+  beauty: number;
+  cost: number;
+  costal: boolean;
+  averageTemp: number;
+}
+
+interface StateWithScore extends State {
+  score: number;
+}
+
+const getColor = (value: number) => {
+  const greenValue = 255 - Math.floor((value / 49) * 255);
+  const redValue = Math.floor((value / 49) * 255);
+
+  return `rgb(${redValue}, ${greenValue}, 0)`;
+};
+
+function OutOf50({ label, value, score }: Record<string, string | number>) {
+  return (
+    <div className="flex justify-between items-center">
+      <p>
+        <span className="font-bold">{label}: </span>
+        <span
+          style={{
+            color: getColor(Number(value)),
+          }}
+        >
+          #{value}
+        </span>
+        <span> out of 50</span>
+      </p>
+      <p>{score}</p>
+    </div>
+  );
+}
+
+function LabelAndValue({
+  label,
+  value,
+  score,
+}: Record<string, string | number | boolean>) {
+  return (
+    <div className="flex justify-between items-center">
+      <p>
+        <span className="font-bold">{label}: </span>
+        {value}
+      </p>
+      <p>{score}</p>
+    </div>
+  );
+}
+
+const costalParam = "prioritize-costal";
+const beautyParam = "prioritize-beauty";
+const costParam = "prioritize-cost";
+const conservativenessParam = "prioritize-conservativeness";
+const averageTempParam = "prioritize-average-temp";
+const bestAverageTempParam = "best-average-temp";
 
 export default function Home() {
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const searchParams = useSearchParams();
+
+  const costalPrioritized =
+    searchParams.get(costalParam)?.toString() === "true";
+  const beautyPrioritized =
+    searchParams.get(beautyParam)?.toString() === "true";
+  const costPrioritized = searchParams.get(costParam)?.toString() === "true";
+  const conservativenessPrioritized =
+    searchParams.get(conservativenessParam)?.toString() === "true";
+  const averageTempPrioritized =
+    searchParams.get(averageTempParam)?.toString() === "true";
+  const bestAverageTemp =
+    Number(searchParams.get(bestAverageTempParam)?.toString()) || 55;
+
+  const [statesSorted, setStatesSorted] = useState<StateWithScore[]>([]);
+
+  const flipHigher = (value: number) => 50 - value;
+
+  const getFlippedValueScore = (value: number, prioritize: boolean) =>
+    flipHigher(value) * (prioritize ? 2 : 1);
+  const getAverageTempScore = (value: number) =>
+    flipHigher(Math.abs(value - bestAverageTemp)) *
+    (averageTempPrioritized ? 2 : 1);
+  const getCostalScore = (value: boolean) =>
+    (value ? 50 : 25) * (costalPrioritized ? 2 : 1);
+
+  const getScore = (state: State) => {
+    const { cost, beauty, conservative, averageTemp, costal } = state;
+    const costScore = getFlippedValueScore(cost, costPrioritized);
+    const beautyScore = getFlippedValueScore(beauty, beautyPrioritized);
+    const conservativeScore = getFlippedValueScore(
+      conservative,
+      conservativenessPrioritized
+    );
+    const averageTempScore = getAverageTempScore(averageTemp);
+
+    const costalScore = getCostalScore(costal);
+    console.log(
+      state.stateName,
+      (costal ? 50 : 25) * (costalPrioritized ? 2 : 1)
+    );
+
+    return (
+      costScore +
+      beautyScore +
+      conservativeScore +
+      averageTempScore +
+      costalScore
+    );
+  };
+
+  useEffect(() => {
+    const newStatesWithScores = states.map((state) => ({
+      ...state,
+      score: getScore(state),
+    }));
+
+    const newSortedStates = newStatesWithScores.sort((a, b) =>
+      a.score > b.score ? -1 : 1
+    );
+
+    setStatesSorted(newSortedStates);
+  }, [
+    costPrioritized,
+    beautyPrioritized,
+    conservativenessPrioritized,
+    averageTempPrioritized,
+    costalPrioritized,
+    bestAverageTemp,
+  ]);
+
+  function handleChange(param: string, value: boolean | string) {
+    const params = new URLSearchParams(searchParams);
+
+    if (value) {
+      params.set(param, value.toString());
+    } else {
+      params.delete(param);
+    }
+
+    replace(`${pathname}?${params.toString()}`, {
+      scroll: false,
+    });
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col mx-auto max-w-6xl p-6 gap-10 my-12">
+      <h1>States Ranker</h1>
+      <div className="flex flex-col gap-2 flex-wrap">
+        <div className="mb-4">
+          <h2>Priorities:</h2>
+          <p>
+            Select what parameters should have a greater affect on the final
+            score.
+          </p>
+        </div>
+        <Checkbox
+          defaultSelected={costalPrioritized}
+          onValueChange={(e) => handleChange(costalParam, e)}
+        >
+          Costal States
+        </Checkbox>
+        <Checkbox
+          defaultSelected={costPrioritized}
+          onValueChange={(e) => handleChange(costParam, e)}
+        >
+          Low Housing Cost
+        </Checkbox>
+        <Checkbox
+          defaultSelected={beautyPrioritized}
+          onValueChange={(e) => handleChange(beautyParam, e)}
+        >
+          Natural Beauty
+        </Checkbox>
+        <Checkbox
+          defaultSelected={conservativenessPrioritized}
+          onValueChange={(e) => handleChange(conservativenessParam, e)}
+        >
+          Conservativeness (Based on 2020 voter data)
+        </Checkbox>
+        <Checkbox
+          defaultSelected={averageTempPrioritized}
+          onValueChange={(e) => handleChange(averageTempParam, e)}
+        >
+          Average Temperate (Fahrenheit)
+        </Checkbox>
+        <div className="max-w-64">
+          <Input
+            min={20}
+            max={80}
+            type="number"
+            defaultValue={bestAverageTemp.toString()}
+            onValueChange={(e) => handleChange(bestAverageTempParam, e)}
+            label="What is the best average Temp?"
+          />
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <Divider />
+      <div className="flex flex-col gap-2">
+        <div className="mb-4">
+          <h2>Result:</h2>
+        </div>
+        <div className="flex justify-between px-4">
+          <p>Data</p>
+          <p>Score</p>
+        </div>
+        <div className="flex flex-col gap-8">
+          {statesSorted.map(
+            (
+              {
+                stateName,
+                costal,
+                conservative,
+                cost,
+                averageTemp,
+                beauty,
+                score,
+              },
+              index
+            ) => (
+              <Card key={stateName} className="bg-zinc-800">
+                <CardHeader className="flex justify-between items-center">
+                  <h2
+                    className="truncate"
+                    style={{
+                      color: getColor(index),
+                    }}
+                  >
+                    {stateName}
+                  </h2>
+                  <h3
+                    className="truncate"
+                    style={{
+                      color: getColor(index),
+                    }}
+                  >
+                    {score.toFixed(2)}
+                  </h3>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                  <OutOf50
+                    label="Conservativeness"
+                    value={conservative}
+                    score={getFlippedValueScore(
+                      conservative,
+                      conservativenessPrioritized
+                    )}
+                  />
+                  <OutOf50
+                    label="Housing Cost"
+                    value={cost}
+                    score={getFlippedValueScore(cost, costPrioritized)}
+                  />
+                  <div className="my-2" />
+                  <div className="flex justify-between items-center">
+                    <p>
+                      <span className="font-bold">Is on the coast: </span>
+                      <span
+                        className={costal ? "text-blue-500" : "text-red-600"}
+                      >
+                        {costal.toString()}
+                      </span>
+                    </p>
+                    <p>{getCostalScore(costal)}</p>
+                  </div>
+                  <OutOf50
+                    label="Natural Beauty"
+                    value={beauty}
+                    score={getFlippedValueScore(beauty, beautyPrioritized)}
+                  />
+                  <LabelAndValue
+                    label="Average Temperature (Fahrenheit)"
+                    value={averageTemp}
+                    score={getAverageTempScore(averageTemp)}
+                  />
+                </CardBody>
+              </Card>
+            )
+          )}
+        </div>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+const { nextui } = require("@nextui-org/react");
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -5,6 +6,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,6 +17,32 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            background: "#FFFFFF",
+            foreground: "#11181C",
+            primary: {
+              foreground: "#FFFFFF",
+              DEFAULT: "#006FEE",
+            },
+          },
+        },
+        dark: {
+          colors: {
+            background: "#171717",
+            foreground: "#f5f5f5",
+            primary: {
+              foreground: "#FFFFFF",
+              DEFAULT: "#006FEE",
+            },
+          },
+        },
+      },
+    }),
+  ],
 };
 export default config;
